33ac314bef47cdfbdca34a3cd42d859b1021ac69
Aušra Gudabskaitė <ausragudabskaite@gmail.com>
2025-05-26 17:40:31 +0300

    fixing mistakes

--- route.h
+++ route.h
@@ -3,10 +3,11 @@
 
 #include <string>
 #include <vector>
-#include <iostream>
+#include <set>
 
 using namespace std;
 
+// Segment structure declaration
 struct Segment {
     string from;
     string to;
@@ -14,27 +15,77 @@ struct Segment {
     double distance;
 };
 
+// Abstract base class Route
 class Route {
 protected:
+    double totalDistance;
     vector<string> path;
-    double totalDistance = 0.0;
 
 public:
+    // Constructor declaration
+    Route();
+    // Virtual destructor declaration
+    virtual ~Route();
+    // Pure virtual method declarations
     virtual void findRoute(const string& from, const string& to, const vector<Segment>& segments) = 0;
-
-    virtual void displayRoute() const {
-        for (size_t i = 0; i < path.size(); ++i) {
-            cout << path[i];
-            if (i != path.size() - 1)
-                cout << " -> ";
-        }
-        cout << "\nTotal Distance: " << totalDistance << " km\n";
-    }
-
-    vector<string> getPath() const { return path; }
-    double getDistance() const { return totalDistance; }
-
-    virtual ~Route() = default; // optional but recommended
+    virtual void displayRoute() const = 0;
+    // Getter method declarations for testing
+    double getDistance() const;
+    vector<string> getPath() const;
 };
 
-#endif
+// Derived class declarations
+class WalkRoute : public Route {
+public:
+    WalkRoute();
+    ~WalkRoute();
+    
+    void findRoute(const string& from, const string& to, const vector<Segment>& segments) override;
+    void displayRoute() const override;
+};
+
+class BikeRoute : public Route {
+public:
+    BikeRoute();
+    ~BikeRoute();
+    
+    void findRoute(const string& from, const string& to, const vector<Segment>& segments) override;
+    void displayRoute() const override;
+};
+
+class CarRoute : public Route {
+public:
+    CarRoute();
+    ~CarRoute();
+    
+    void findRoute(const string& from, const string& to, const vector<Segment>& segments) override;
+    void displayRoute() const override;
+};
+
+// NavigationApp class declaration
+class NavigationApp {
+private:
+    Route* strategy;
+    
+public:
+    NavigationApp(Route* strategy);
+    ~NavigationApp();
+    
+    void planRoute(const string& from, const string& to);
+    vector<Segment> getAllSegments();
+};
+
+// TestRoute class declaration (for testing purposes)
+class TestRoute : public Route {
+protected:
+    set<string> allowedTerrains;
+    
+public:
+    TestRoute(const set<string>& terrains);
+    ~TestRoute();
+    
+    void findRoute(const string& from, const string& to, const vector<Segment>& segments) override;
+    void displayRoute() const override;
+};
+
+#endif // ROUTE_H
