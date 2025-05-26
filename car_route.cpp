c7de7dcbae7b7231e5eb1d3fe1833a93b77e4f8b
Aušra Gudabskaitė <ausragudabskaite@gmail.com>
2025-05-26 17:39:09 +0300

    fixing mistakes

--- car_route.cpp
+++ car_route.cpp
@@ -1,5 +1,3 @@
-// car_route.cpp
-
 #include "route.h"
 #include <iostream>
 #include <set>
@@ -7,41 +5,40 @@
 
 using namespace std;
 
-class CarRoute : public Route {
-    double totalDistance = 0;
-    vector<string> path;
+CarRoute::CarRoute() {
+    totalDistance = 0;
+}
 
-public:
-    void findRoute(const string& from, const string& to, const vector<Segment>& segments) override {
-        set<string> allowedTerrains = {"road", "highway"};
-        totalDistance = 0;
-        path.clear();
+CarRoute::~CarRoute() {}
 
-        for (const auto& seg : segments) {
-            if (allowedTerrains.count(seg.terrain)) {
-                if (path.empty() && seg.from == from) {
-                    path.push_back(seg.from);
-                    path.push_back(seg.to);
-                    totalDistance += seg.distance;
-                } else if (!path.empty() && path.back() == seg.from) {
-                    path.push_back(seg.to);
-                    totalDistance += seg.distance;
-                    if (seg.to == to) break;
-                }
+void CarRoute::findRoute(const string& from, const string& to, const vector<Segment>& segments) {
+    set<string> allowedTerrains = {"road", "highway"};
+    totalDistance = 0;
+    path.clear();
+
+    for (const auto& seg : segments) {
+        if (allowedTerrains.count(seg.terrain)) {
+            if (path.empty() && seg.from == from) {
+                path.push_back(seg.from);
+                path.push_back(seg.to);
+                totalDistance += seg.distance;
+            } else if (!path.empty() && path.back() == seg.from) {
+                path.push_back(seg.to);
+                totalDistance += seg.distance;
+                if (seg.to == to) break;
             }
         }
     }
+}
 
-    void displayRoute() const override {
-        if (!path.empty() && path.front() != path.back()) {
-            for (const auto& loc : path) cout << loc << " -> ";
-            cout << "END\n";
-            cout << fixed << setprecision(0); // This ensures no decimal points
-            cout << "Total distance: " << totalDistance << " km\n";
-            cout << "Estimated time: " << (totalDistance / 70) * 60 << " min\n\n";
-        } else {
-            cout << "No route found.\n\n";
-        }
+void CarRoute::displayRoute() const {
+    if (!path.empty() && path.front() != path.back()) {
+        for (const auto& loc : path) cout << loc << " -> ";
+        cout << "END\n";
+        cout << fixed << setprecision(0); 
+        cout << "Total distance: " << totalDistance << " km\n";
+        cout << "Estimated time: " << (totalDistance / 70) * 60 << " min\n\n";
+    } else {
+        cout << "No route found.\n\n";
     }
-};
-
+}
